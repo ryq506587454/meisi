0df0b37e8f0ecc4045b4db65245b2a3d2e41eeee
ryq506587454 <38856689+ryq506587454@users.noreply.github.com>
2018-06-27 15:49:59 +0800

    20180627

--- pages/user/page/noticeDetail.js
+++ pages/user/page/noticeDetail.js
@@ -5,7 +5,8 @@ Page({
    * 页面的初始数据
    */
   data: {
-  notice:{}
+  notice:{},
+  publishTime:null
   },
 
   /**
@@ -23,59 +24,15 @@ Page({
         'content-type': 'application/x-www-form-urlencoded'
       },
       success: function (res) {
+        console.log(res)
         self.setData({
           notice: res.data
+        });
+        var a = self.data.notice.publishTime.slice(0, 10);
+        self.setData({
+          publishTime:a
         })
       }
     })
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
   }
 })
